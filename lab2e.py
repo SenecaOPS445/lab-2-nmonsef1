84894015933ea84733e25ae172d4e2e4f9cd4212
Niyayesh Monsef <nmonsef@myseneca.ca>
2024-05-22 08:52:05 -0400

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Author:  niyayesh monsef
+Author ID:  nmonsef
+Date Created:  2024/05/22
+
+'''
+
+count = 10
+while count != 0: 
+    print(count)
+    count = count   - 1
+
+print('blast off!')
