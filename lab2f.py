a8529153e62680b7e5b61ac7f4eb30f2f0c7e918
Niyayesh Monsef <nmonsef@myseneca.ca>
2024-05-22 08:52:29 -0400

    added lab2f

---
+++ lab2f.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Author:  niyayesh monsef
+Author ID:  nmonsef
+Date Created:  2024/05/22
+
+'''
+
+import sys
+
+count = int(sys.argv[1])
+
+while count != 0:
+    print(count)
+    count = count   - 1
+
+print('blast off!')
